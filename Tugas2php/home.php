3b1b945b5accf6862f720b181d1ec3fd6f7fd3b2
dayati <dayatidirwan@gmail.com>
2024-03-21 22:25:35 +0700

    tugas pratikum

---
+++ Tugas2php/home.php
@@ -0,0 +1,6 @@
+
+		<h1>Welcome Home!</h1>
+			<p>
+                <span>Dayati</span> - <span>0110123304</span> - <span>SI02</span>
+			</p>
+               
---
+++ Web2024/si022024/tugas/data_nilai.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+ <div class="container">
+  <h2>Data Mahasiswa</h2>
+  <table class="table">
+      <thead>
+          <tr>
+              <th>No</th>
+              <th>Nama Lengkap</th>
+              <th>Mata Kuliah</th>
+              <th>UTS</th>
+              <th>UAS</th>
+              <th>Tugas</th>
+              <th>Rerata</th>
+              <th>Grade</th>
+              <th>Predikat</th>
+              <th>Keterangan</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td>1</td>
+              <td>Dayati</td>
+              <td>Statistika dan Probabilitas</td>
+              <td>89</td>
+              <td>95</td>
+              <td>80</td>
+              <td>87.95</td>
+              <td>A</td>
+              <td>Sangat Memuaskan</td>
+              <td>Lulus</td>
+          </tr>
+          <tr>
+              <td>2</td>
+              <td>Aya</td>
+              <td>Statistika dan Probabilitas</td>
+              <td>78</td>
+              <td>81</td>
+              <td>80</td>
+              <td>79.75</td>
+              <td>B</td>
+              <td>Memuaskan</td>
+              <td>Lulus</td>
+          </tr>
+
+          <?php
+			 
+                 
+          ?>
+      </tbody>
+  </table>
+</div>
